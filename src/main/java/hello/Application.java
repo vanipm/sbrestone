e41c419cc47aa00ab83a6a2e5d6922cebdedef14
waveamiotb <waveam.iot@gmail.com>
2017-03-06 15:10:19 -0500

    war loading changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,7 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+    {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
