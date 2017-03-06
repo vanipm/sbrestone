94d3182c2af536c411103b90b1be23e0a9eaeb90
waveamiotb <waveam.iot@gmail.com>
2017-03-06 17:52:17 -0500

    ref updates

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServeletInitializer;
 
+
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
