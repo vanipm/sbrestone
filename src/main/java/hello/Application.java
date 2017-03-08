f602e6dc03bfe2985cf92862476c335e6748e6c5
waveamiotb <waveam.iot@gmail.com>
2017-03-08 16:48:12 -0500

    spring boot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,7 +3,7 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServeletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
